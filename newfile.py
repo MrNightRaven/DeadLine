8b4eab05073067ac047d6f57e4dddc112ca4f385
Mike Frederick <36541416+trevorfs@users.noreply.github.com>
2018-04-26 14:45:41 +0700

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,102 +1,423 @@
+# -*- coding: utf-8 -*-
 from linepy import *
 from akad import *
 from akad import TalkService
-from time import sleep
-from bs4 import BeautifulSoup
-from datetime import datetime
-from multiprocessing import Process, Pool
-import time, random, sys, json, codecs, ast, threading, glob, re, string, os, requests, subprocess, six, atexit, datetime, multiprocessing, pytz
-from datetime import datetime
-import locale
-from urllib import parse as urlparse
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
-import six; from six.moves import cPickle as pickle
-import os, random, psutil, time, threading
-import numpy as np
-from threading import Thread
 
-mike = LINE("TOKEN KAMU")#kosongin aja kalau pengen login qr
-mike.log("Auth Token: " + str(mike.authToken))
+trev = LINE()
+trev.log("Ini Auth Token mu: {}".format(str(channel.getChannelResult())))
 
-oepoll = OEPoll(mike)
-simike = mike.getProfile().mid
+trev2 = LINE()
+trev2.log("In Auth Token TREVOR2: {}".format(str(channel.getChannelResult())))
 
-pergrup = ["disini diisi gid/groupid. dikosongin juga gapapa"] #ini dipake buat yg mau botnya aktif cuman pergrup bukan global/menyeluruh
+trev3 = LINE()
+trev3.log("In Auth Token TREVOR3: {}".format(str(channel.getChannelResult())))
+trev4 = LINE()
+trev4.log("In Auth Token TREVOR4: {}".format(str(channel.getChannelResult())))
+oepoll = OEPoll(trev)
+cl = trev
+ca = trev2
+cs = trev3
+cz = trev4
+KAC=[cl,ca,cs,cz]
+A3V=[ca,cs,cz]
+KAV=[ca,cs]
+mid = cl.getProfile().mid
+Amid = ca.getProfile().mid
+Bmid = cs.getProfile().mid
+Cmid = cz.getProfile().mid
+Bots=[mid,Amid,Bmid,Cmid]
+admin=[]
 
-
-
-#Udah ya langsung aja ya simple kok ;)#
-#Yang mo ngejudge hargai hasil usaha orang
-
-while True:
+def mention(to, nama):
+    aa = ""
+    bb = ""
+    strt = int(14)
+    akh = int(14)
+    nm = nama
+    for mm in nm:
+      akh = akh + 2
+      aa += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(mm)+"},"""
+      strt = strt + 6
+      akh = akh + 4
+      bb += "\xe2\x95\xa0 @x \n"
+    aa = (aa[:int(len(aa)-1)])
+    msg = Message()
+    msg.to = to
+    msg.text = "\xe2\x95\x94\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\n"+bb+"\xe2\x95\x9a\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90"
+    msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+aa+']}','EMTVER':'4'}
+    print("[Command] Tag All")
+    try:
+       cl.sendMessage(msg)
+    except Exception as error:
+       print(error)
+def mice(op):
 	try:
-		ops = oepoll.singleTrace(count=50)
-		if ops is not None:
-			for op in ops:
-				if op.type == 25:
-					if op.param1 in pergrup:
-						msg = op.message()
-						pesanku = msg.text
-						msg_id = msg.id
-						buatsiska = msg.to
-						darimike = msg._from
-						try:
-							if msg.contentType == 0:
-								if "help" == pesanku.lower():
-									siskacantik = "Selfbot Version\n"
-									siskacantik += "Speed"
-									mike.sendMessage(buatsiska, siskacantik)
-							if "speed" == pesanku.lower():
-								aa = time.time()
-								mike.sendMessage(buatsiska,"wait mike")
-								ab = time.time() - aa
-								mike.sendMessage(buatsiska, "%s " % (ab))
-							if "restart" == pesanku.lower():
-								mike.sendMessage(buatsiska, "Restart Program")
-								python = sys.executable
-								os.execl(python, python, *sys.argv)
-								
-								
-						except Exception as error:
-							trev.sendMessage(buatsiska, error)
-			if op.type == 25:
-				msg = op.message
-				pesanku = msg.text
-				msg_id = msg.id
-				buatsiska = msg.to
-				darimike = msg._from
+		if op.type == 13:
+			if op.param3 in mid:
+				if op.param2 in Amid:
+					G = ca.getGroup(op.param1)
+					G.preventJoinByTicket = False
+					ca.updateGroup(G)
+					Ticket = ca.reissueGroupTicket(op.param1)
+					cl.acceptGroupInvitationByTicket(op.param1,Ticket)
+					G.preventJoinByTicket = True
+					ca.updateGroup(G)
+					Ticket = ca.reissueGroupTicket(op.param1)
+			if op.param3 in Amid:
+				if op.param2 in Bmid:
+					X = cs.getGroup(op.param1)
+					X.preventJoinByTicket = False
+					cs.updateGroup(X)
+					Ti = cs.reissueGroupTicket(op.param1)
+					ca.acceptGroupInvitationByTicket(op.param1,Ti)
+					X.preventJoinByTicket = True
+					cs.updateGroup(X)
+					Ti = cs.reissueGroupTicket(op.param1)
+			if op.param3 in Bmid:
+				if op.param2 in Cmid:
+					X = cz.getGroup(op.param1)
+					X.preventJoinByTicket = False
+					cz.updateGroup(X)
+					Ti = cz.reissueGroupTicket(op.param1)
+					cs.acceptGroupInvitationByTicket(op.param1,Ti)
+					X.preventJoinByTicket = True
+					cz.updateGroup(X)
+					Ti = cz.reissueGroupTicket(op.param1)
+					
+			if op.param3 in Cmid:
+				if op.param2 in mid:
+					X = cl.getGroup(op.param1)
+					X.preventJoinByTicket = False
+					cl.updateGroup(X)
+					Ti = cl.reissueGroupTicket(op.param1)
+					cz.acceptGroupInvitationByTicket(op.param1,Ti)
+					X.preventJoinByTicket = True
+					cl.updateGroup(X)
+					Ti = cl.reissueGroupTicket(op.param1)
+		if op.type == 19:
+			if mid in op.param3:
+				if op.param2 not in Bots and admin:
+					pass
 				try:
-					if msg.contentType == 0:
-						if "bot:on" == pesanku.lower():
-							if msg.to in pergrup:
-								siskacantik = mike.getGroup(msg.to)
-								aa = "STATUS: \n Already Trev"
-								mike.sendMessage(buatsiska, aa)
+					ca.kickoutFromGroup(op.param1,[op.param2])
+				except:
+					try:
+						random.choice(A3V).kickoutFromGroup(op.param1,[op.param2])
+						random.choice(A3V).inviteIntoGroup(op.param1,[op.param3])
+					except:
+						print ("client Kick regulation or Because it does not exist in the group、\n["+op.param1+"]\nの\n["+op.param2+"]\nを蹴る事ができませんでした。\nブラックリストに追加します。")
+						if op.param2 in blacklist:
+							pass
+						else:
+							blacklist.append(op.param2)
+							G = ca.getGroup(op.param1)
+							G.preventJoinByTicket = False
+							ca.updateGroup(G)
+							Ti = ca.reissueGroupTicket(op.param1)
+							cl.acceptGroupInvitationByTicket(op.param1,Ti)
+							cs.acceptGroupInvitationByTicket(op.param1,Ti)
+							cz.acceptGroupInvitationByTicket(op.param1,Ti)
+							X = ca.getGroup(op.param1)
+							X.preventJoinByTicket = True
+							ca.updateGroup(X)
+							Ti = ca.reissueGroupTicket(op.param1)
+							if op.param2 in blacklist:
+								pass
 							else:
-								siskacantik = mike.getGroup(msg.to)
-								pergrup.append(msg.to)
-								simike = "\nAbout:\n BOT MODE AKTIF HANYA DI GROUP {}".format(siskacantik)
-								mike.sendMessage(buatsiska,simike)
-						if "bot:off" == pesanku.lower():
-							if msg.to in pergrup:
-								siskaa = mike.getGroup(msg.to)
-								pergrup.remove(msg.to)
-								mikey = "BOT AKAN DI NON AKTIFKAN DI GROUP {}".format(siskaa)
-								mike.sendMessage(buatsiska, mikey)
-							else:
-								siskaa = mike.getGroup(msg.to)
-								cantik = "Already Remove or not registerd"
-								mike.sendMessage(buatsiska, cantik)
+								blacklist.append(op.param2)
 								
-							
-				except Exception as error:
-					print(error)
-			oepoll.setRevision(op.revision)
+			if Amid in op.param3:
+				if op.param2 not in Bots and admin:
+					pass
+				try:
+					cs.kickoutFromGroup(op.param1,[op.param2])
+				except:
+					try:
+						random.choice(A3V).kickoutFromGroup(op.param1,[op.param2])
+						random.choice(A3V).inviteIntoGroup(op.param1,[op.param3])
+					except:
+						print ("clientが蹴り規制orグループに存在しない為、\n["+op.param1+"]\nの\n["+op.param2+"]\nを蹴る事ができませんでした。\nブラックリストに追加します。")
+						print ("clientが蹴り規制orグループに存在しない為、\n["+op.param1+"]\nの\n["+op.param2+"]\nを蹴る事ができませんでした。\nブラックリストに追加します。")
+					if op.param2 in blacklist:
+						pass
+					else:
+						blacklist.append(op.param2)
+						X = cs.getGroup(op.param1)
+						X.preventJoinByTicket = False
+						cs.updateGroup(X)
+						Ti = cs.reissueGroupTicket(op.param1)
+						cl.acceptGroupInvitationByTicket(op.param1,Ti)
+						ca.acceptGroupInvitationByTicket(op.param1,Ti)
+						cz.acceptGroupInvitationByTicket(op.param1,Ti)
+						G = cs.getGroup(op.param1)
+						G.preventJoinByTicket = True
+						cs.updateGroup(G)
+						Ticket = cs.reissueGroupTicket(op.param1)
+						if op.param2 in blacklist:
+							pass
+						else:
+							blacklist.append(op.param2)
+			if Bmid in op.param3:
+				if op.param2 not in Bots and admin:
+					pass
+				try:
+					cz.kickoutFromGroup(op.param1,[op.param2])
+				except:
+					try:
+						random.choice(A3V).kickoutFromGroup(op.param1,[op.param2])
+						random.choice(A3V).inviteIntoGroup(op.param1,[op.param3])
+					except:
+						print ("clientが蹴り規制orグループに存在しない為、\n["+op.param1+"]\nの\n["+op.param2+"]\nを蹴る事ができませんでした。\nブラックリストに追加します。")
+						if op.param2 in blacklist:
+							pass
+						else:
+							blacklist.append(op.param2)
+							X = cz.getGroup(op.param1)
+							X.preventJoinByTicket = False
+							cz.updateGroup(X)
+							Ti = cz.reissueGroupTicket(op.param1)
+							cl.acceptGroupInvitationByTicket(op.param1,Ti)
+							ca.acceptGroupInvitationByTicket(op.param1,Ti)
+							cs.acceptGroupInvitationByTicket(op.param1,Ti)
+							G = cz.getGroup(op.param1)
+							G.preventJoinByTicket = True
+							cz.updateGroup(G)
+							Ticket = cz.reissueGroupTicket(op.param1)
+							if op.param2 in blacklist:
+								pass
+							else:
+								blacklist.append(op.param2)
+								
+			if Cmid in op.param3:
+				if op.param2 not in Bots and admin:
+					pass
+				try:
+					cl.kickoutFromGroup(op.param1,[op.param2])
+				except:
+					try:
+						random.choice(A3V).kickoutFromGroup(op.param1,[op.param2])
+						random.choice(A3V).inviteIntoGroup(op.param1,[op.param3])
+					except:
+						print ("clientが蹴り規制orグループに存在しない為、\n["+op.param1+"]\nの\n["+op.param2+"]\nを蹴る事ができませんでした。\nブラックリストに追加します。")
+						if op.param2 in blacklist:
+							pass
+						else:
+							blacklist.append(op.param2)
+							X = ca.getGroup(op.param1)
+							X.preventJoinByTicket = False
+							ca.updateGroup(X)
+							Ti = ca.reissueGroupTicket(op.param1)
+							cl.acceptGroupInvitationByTicket(op.param1,Ti)
+							cs.acceptGroupInvitationByTicket(op.param1,Ti)
+							cz.acceptGroupInvitationByTicket(op.param1,Ti)
+							G = cz.getGroup(op.param1)
+							G.preventJoinByTicket = True
+							cz.updateGroup(G)
+							Ticket = cz.reissueGroupTicket(op.param1)
+							if op.param2 in blacklist:
+								pass
+							else:
+								blacklist.append(op.param2)
+								
+		if op.type == 13:
+			if op.param2 not in Bots:
+				if op.param2 not in admin:
+					if op.param3 not in admin:
+						if op.param1 in settingTamu:
+							blacklist.append(op.param2)
+							group = cl.getGroup(op.param1)
+							random.choice(A3V).cancelGroupInvitation(op.param1,[op.param3])
+							random.choice(A3V).kickoutFromGroup(op.param1,[op.param2])
 						
+		if op.type == 19:
+			if op.param2 not in Bots:
+				if op.param2 not in admin:
+					if op.param3 in admin:
+						if op.param1 in settingProtect:
+							try:
+								mybot=[cl,ca,cs,cz]
+								asw = random.choice(mybot)
+								blacklist.append(op.param2)
+								asw.kickoutFromGroup(op.param1,[op.param2])
+								inviteIntoGroup(op.param1,[op.param3])
+							except Exception as error:
+								print(error)
+					
+		if op.type == 32:
+			if op.param2 not in Bots:
+				if op.param2 not in admin:
+					if op.param3 not in admin:
+						if op.param1 in settingCancel:
+							blacklist.append(op.param2)
+							group = cl.getGroup(op.param1)
+							random.choice(A3V).cancelGroupInvitation(op.param1, [op.param3])
+							random.choice(A3V).kickoutFromGroup(op.param1,[op.param2])
+						
+		if op.type == 13:
+			if op.param3 in mid:
+				if op.param2 in admin:
+					G = cl.getGroup(op.param1)
+					cl.acceptGroupInvitation(op.param1)
+					G.preventJoinByTicket = False
+					cl.updateGroup(G)
+					invsend = 0
+					Ti = cl.reissueGroupTicket(op.param1)
+					ca.acceptGroupInvitationByTicket(op.param1,Ti)
+					cs.acceptGroupInvitationByTicket(op.param1,Ti)
+					cz.acceptGroupInvitationByTicket(op.param1,Ti)
+					G = cz.getGroup(op.param1)
+					G.preventJoinByTicket = True
+					cz.updateGroup(G)
+					cz.reissueGroupTicket(op.param1)
+					
+		if op.type == 11:
+			if op.param2 not in Bots:
+				if op.param2 not in admin:
+					if op.param1 in settingQr:
+						blacklist.append(op.param2)
+						G = cl.getGroup(op.param1)
+						G.preventJoinByTicket = True
+						random.choice(A3V).kickoutFromGroup(op.param1,[op.param2])
+						random.choice(A3V).updateGroup(G)
+		if op.type == 26:
+			msg = op.message
+			pesanku = msg.text
+			msg_id = msg.id
+			receiver = msg.to
+			darimike = msg._from
+			try:
+				if msg.contentType == 0:
+					if "linkprotect:on" == pesan.lower():
+						if msg._from in admin:
+							if msg.to in settingQr:
+								siska = cl.getGroup(msg.to)
+								cl.sendMessage(msg.to,"「􏿿 Protect Link 」\n Protect Link Already Active in Group {}".format(siska.name))
+								ca.sendMessage(msg.to,"「􏿿 Protect Link 」\n Protect Link Already Active in Group {}".format(siska.name))
+								cs.sendMessage(msg.to,"「􏿿 Protect Link 」\n Protect Link Already Active in Group {}".format(siska.name))
+								cz.sendMessage(msg.to,"「􏿿 Protect Link 」\n Protect Link Already Active")
+							else:
+								settingQr.append(msg.to)
+							siska = cl.getGroup(msg.to)
+							cl.sendText(msg.to,"「􏿿 Protect Link 」\n Protect Link Has Been Activated in Group {}".format(siska.name))
+				if "linkprotect:off" == pesan.lower():
+					if msg._from in admin:
+						if msg.to in settingQr:
+							siska = cl.getGroup(msg.to)
+							settingQr.remove(msg.to)
+							cl.sendMessage(msg.to,"「􏿿 Protect Link 」\n Protect Link Has Been Non-Activated in Group {}".format(siska.name))
+						else:
+							cl.sendMessage(msg.to,"「􏿿 Protect Link 」\n Protect Link Already Non-Active")
+					if "cancelprotect:on" == pesan.lower():
+						if msg._from in admin:
+							if msg.to in settingCancel:
+								cl.sendMessage(msg.to,"「􏿿 Protect Cancel 」\n Protect Cancel Already Active")
+							else:
+								settingCancel.append(msg.to)
+								cl.sendMessage(msg.to,"「􏿿 Protect Cancel 」\n Protect Cancel Has Been Activated")
+					if "cancelprotect:off" == pesan.lower():
+						if msg._from in admin:
+							if msg.to in settingCancel:
+								settingCancel.remove(msg.to)
+								cl.sendMessage(msg.to,"「􏿿 Protect Cancel 」\n Protect Cancel Has Been Non-Activated")
+							else:
+								cl.sendMessage(msg.to,"「􏿿 Protect Cancel 」\n Protect Cancel Already Non-Active")
+					if "kickprotect:on" == pesan.lower():
+						if msg._from in admin:
+							if msg.to in settingProtect:
+								cl.sendMessage(msg.to,"「􏿿 Protect Kick 」\n Protect Kick Already Active")
+							else:
+								settingProtect.append(msg.to)
+								cl.sendMessage(msg.to,"「􏿿 Protect Kick 」\n Protect Kick Has Been Activated")
+					if "kickprotect:off" == pesan.lower():
+						if msg._from in admin:
+							if msg.to in settingProtect:
+								settingProtect.remove(msg.to)
+								cl.sendMessage(msg.to,"「􏿿 Protect Kick 」\n Protect Has Been Non-Activated")
+							else:
+								cl.sendMessage(msg.to,"「􏿿 Protect Kick 」\n Protect Kick Already Non-Active")
+								
+					if "inviteprotect:on" == pesan.lower():
+						if msg._from in admin:
+							if msg.to in settingTamu:
+								cl.sendMessage(msg.to,"「􏿿 Protect Invite 」\n Protect Invite Already Active")
+							else:
+								settingTamu.append(msg.to)
+								cl.sendMessage(msg.to,"「􏿿 Protect Invite 」\n Protect Invite Has Been Activated")
+								
+					if "inviteprotect:off" == pesan.lower():
+						if msg._from in admin:
+							if msg.to in settingTamu:
+								settingGuest.remove(msg.to)
+								cl.sendMessage(msg.to,"「􏿿 Protect Invite 」\n Protect Invite Has Been Non-Activated")
+							else:
+								cl.sendMessage(msg.to,"「􏿿 Protect Invite 」\n Protect Invite Already Non-Active")
+					if "url:off" == pesan.lower():
+						if msg._from in admin:
+							aa = cl.getGroup(msg.to)
+							aa.preventedJoinByTicket = True
+							cl.updateGroup(aa)
+							cl.sendMessage(msg.to,"「􏿿 Link Group 」\n⚔ Status: Blocked QR in Group {}".format(aa.name))
+						else:
+							cl.sendMessage(msg.to,"You're Not Admin")
+					if "" == pesan.lower():
+						if msg._from in admin:
+							X = cl.getGroup(msg.to)
+							X.preventedJoinByTicket = True
+							cl.updateGroup(X)
+							cl.sendMessage(msg.to,"「􏿿 Link Group 」\n⚔ Status: Allowed QR in Group {}".format(X.name))
+						else:
+							cl.sendMessage(msg.to,"You're Not Admin")
+					if "help" == pesan.lower():
+						cl.sendMesage(msg.to,helpmsg)
+					if "trevmasuk" == pesan.lower():
+						if msg._from in admin:
+							G = cl.getGroup(msg.to)
+							ginfo = cl.getGroup(msg.to)
+							G.preventedJoinByTicket = False
+							cl.updateGroup(G)
+							invsend = 0
+							Ticket = cl.reissueGroupTicket(msg.to)
+							ca.acceptGroupInvitationByTicket(msg.to,Ticket)
+							cs.acceptGroupInvitationByTicket(msg.to,Ticket)
+							cz.acceptGroupInvitationByTicket(msg.to,Ticket)
+							G = cz.getGroup(msg.to)
+							G.preventJoinByTicket = True
+							cz.updateGroup(G)
+							ca.sendMessage(msg.to,"Halo ! "+ str(ginfo.name))
+							cs.sendMessage(msg.to,"Halo ! "+ str(ginfo.name))
+							cz.sendMessage(msg.to,"Halo ! "+ str(ginfo.name))
+							print("kicker ok")
+							G.preventJoinByTicket(G)
+							cz.updateGroup(G)
+					if "trevbye" == pesan.lower():
+						if msg._from in admin:
+							ginfo = cl.getGroup(msg.to)
+							try:
+								cl.sendMessage(msg.to,"Bye ! "+ str(ginfo.name))
+								ca.sendMessage(msg.to,"Bye ! "+ str(ginfo.name))
+								cs.sendMessage(msg.to,"Bye ! "+ str(ginfo.name))
+								cz.sendMessage(msg.to,"Bye ! "+ str(ginfo.name))
+								cl.leaveGroup(msg.to)
+								ca.leaveGroup(msg.to)
+								cs.leaveGroup(msg.to)
+								cz.leaveGroup(msg.to)
+							except:
+								pass
+			except Exception as error:
+				print(error)
 	except Exception as error:
 		print(error)
-		
-#Thanks ;) 
+
+def thread():
+	while True:
+		try:
+			ops = oepoll.singleTrace(count=50)
+			if ops is not None:
+				for op in ops:
+					mice(op)
+					oepoll.setRevision(op.revision)
+		except Exception as e:
+			print(e)
+			
+Thread(target=thread()).start()
+print("Started")
